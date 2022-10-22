44f10d2f319f16ed07bda74f3d021e8bbfd61d45
tsulai <sulaimyint@hotmail.com>
2022-10-22 14:30:35 +0800

    dinnerdecider app done

--- Assignment2/DinnerDecider/app/src/main/java/com/miu/dinnerdecider/MainActivity.kt
+++ Assignment2/DinnerDecider/app/src/main/java/com/miu/dinnerdecider/MainActivity.kt
@@ -2,10 +2,22 @@ package com.miu.dinnerdecider
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var foodList = arrayListOf("Hamburger", "Pizza", "Mexican", "American", "Chinese")
+        tvFoodName.text = foodList[(0..4).random()]
+        btnDecide.setOnClickListener{
+            tvFoodName.text = foodList[(0..4).random()]
+        }
+        btnAddFood.setOnClickListener{
+            if(etAddNewFood.text.toString()!== "add new food"){
+                foodList.add(etAddNewFood.text.toString())
+                println(foodList.toString())
+            }
+        }
     }
 }
